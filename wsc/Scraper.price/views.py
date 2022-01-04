16085616040e273f7a638f99899d44c5983e2d98
Anusha Mehrin <anusha.mehrin@northsouth.edu>
2022-01-04 23:43:23 +0600

    up

---
+++ wsc/Scraper.price/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from bs4 import BeautifulSoup
+import requests
+
+
+headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9'}
+url='https://www.reddit.com/r/programming/'
+response=requests.get(url,headers=headers)
+
+
+soup=BeautifulSoup(response.content,'lxml')
+
+#print(soup.select('.Post')[0].get_text())
+
+for item in soup.select('.Post'):
+	try:
+		print('----------------------------------------')
+		print(item.select('._1rZYMD_4xY3gRcSS3p8ODO')[0].get_text())
+		print(item.select('._eYtD2XCVieq6emjKBH3m')[0].get_text())
+		print(item.select('.FHCV02u6Cp2zYL0fhQPsO')[0].get_text())
+ 		#print(item.select('._2INHSNB8V5eaWp4P0rY_mE a[href]')[0]['href'])
+
+	except Exception as e:
+		#raise e
+		print('')
